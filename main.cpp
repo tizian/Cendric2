c0a636e8459c77ec3d9970ec789ee803297db444
Ironbell <isabelleroesch@msn.com>
2015-03-06 00:20:54 +0100

    The second commit

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[]) {
 
   /* Code adapted from the SFML 2 "Window" example */
 
-  sf::Window App(sf::VideoMode(800, 600), "myproject");
+  sf::Window App(sf::VideoMode(800, 640), "myproject");
 
   while (App.isOpen()) {
     sf::Event Event;
