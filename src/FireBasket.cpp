e0e0606d3e447730fe5d09bde2a1dfdb4cd2f9f5
Ironbell <isabelleroesch@msn.com>
2015-04-27 18:36:47 +0200

    made fire loop look nicer

--- src/FireBasket.cpp
+++ src/FireBasket.cpp
@@ -12,22 +12,10 @@ void FireBasket::load()
 
 	Animation idleAnimation;
 	idleAnimation.setSpriteSheet(g_resourceManager->getTexture(ResourceID::Texture_screen_splash_fireanimation));
-	idleAnimation.addFrame(sf::IntRect(0, 0, 192, 600));
-	idleAnimation.addFrame(sf::IntRect(192, 0, 192, 600));
-	idleAnimation.addFrame(sf::IntRect(192 * 2, 0, 192, 600));
-	idleAnimation.addFrame(sf::IntRect(192 * 3, 0, 192, 600));
-	idleAnimation.addFrame(sf::IntRect(192 * 4, 0, 192, 600));
-	idleAnimation.addFrame(sf::IntRect(192 * 5, 0, 192, 600));
-	idleAnimation.addFrame(sf::IntRect(192 * 6, 0, 192, 600));
-	idleAnimation.addFrame(sf::IntRect(192 * 7, 0, 192, 600));
-	idleAnimation.addFrame(sf::IntRect(192 * 8, 0, 192, 600));
-	idleAnimation.addFrame(sf::IntRect(192 * 9, 0, 192, 600));
-	idleAnimation.addFrame(sf::IntRect(192 * 10, 0, 192, 600));
-	idleAnimation.addFrame(sf::IntRect(192 * 11, 0, 192, 600));
-	idleAnimation.addFrame(sf::IntRect(192 * 12, 0, 192, 600));
-	idleAnimation.addFrame(sf::IntRect(192 * 13, 0, 192, 600));
-	idleAnimation.addFrame(sf::IntRect(192 * 14, 0, 192, 600));
-
+	for (int i = 0; i < 16; i++)
+	{
+		idleAnimation.addFrame(sf::IntRect(192 * i, 0, 192, 600));
+	}
 	addAnimation(GameObjectState::Idle, idleAnimation);
 
 	setFrameTime(sf::seconds(0.1f));
