1444cbe55626ef2f970d7ba46c52e203d8c75ca6
tizian <tizian.zeltner@gmail.com>
2015-09-19 01:06:54 +0200

    Small hack to fix light problems with transparent foreground layers

--- src/Screens/MapScreen.cpp
+++ src/Screens/MapScreen.cpp
@@ -4,36 +4,36 @@
 using namespace std;
 
 const std::string vertexShader = \
-"void main()" \
-"{" \
-"    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;" \
-"    gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;" \
-"    gl_FrontColor = gl_Color;" \
-"}";
+	"void main()" \
+	"{" \
+	"    gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;" \
+	"    gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;" \
+	"    gl_FrontColor = gl_Color;" \
+	"}";
 
 const std::string lightFragmentShader = \
-"uniform sampler2D texture;" \
-"uniform float ambientLevel;" \
-"" \
-"void main()" \
-"{" \
-"    vec4 pixel = texture2D(texture, gl_TexCoord[0].xy);" \
-"    float lightLevel = max(0.0, ambientLevel - pixel.r);" \
-"    gl_FragColor = vec4(0.0, 0.0, 0.0, lightLevel);" \
-"}";
+	"uniform sampler2D texture;" \
+	"uniform float ambientLevel;" \
+	"" \
+	"void main()" \
+	"{" \
+	"    vec4 pixel = texture2D(texture, gl_TexCoord[0].xy);" \
+	"    float lightLevel = max(0.0, ambientLevel - pixel.r);" \
+	"    gl_FragColor = vec4(0.0, 0.0, 0.0, lightLevel);" \
+	"}";
 
 const std::string foregroundFragmentShader = \
-"uniform sampler2D texture;" \
-"uniform float ambientLevel;" \
-"" \
-"void main()" \
-"{" \
-"    vec4 pixel = texture2D(texture, gl_TexCoord[0].xy);" \
-"    if (pixel.a == 0.0)" \
-"        gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);" \
-"    else" \
-"        gl_FragColor = vec4(0.0, 0.0, 0.0, ambientLevel);" \
-"}";
+	"uniform sampler2D texture;" \
+	"uniform float ambientLevel;" \
+	"" \
+	"void main()" \
+	"{" \
+	"    vec4 pixel = texture2D(texture, gl_TexCoord[0].xy);" \
+	"    if (pixel.a == 1.0)" \
+	"		gl_FragColor = vec4(0.0, 0.0, 0.0, ambientLevel); " \
+	"    else" \
+	"        gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);" \
+	"}";
 
 MapScreen::MapScreen(const std::string& mapID, CharacterCore* core) : GameScreen(core)
 {
