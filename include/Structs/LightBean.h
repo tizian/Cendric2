2f43eb2226c04e05758c3b3c864b66102b17db06
Ironbell <isabelleroesch@msn.com>
2015-09-20 00:44:10 +0200

    added brightness for lights

--- include/Structs/LightBean.h
+++ include/Structs/LightBean.h
@@ -5,9 +5,12 @@
 struct LightBean
 {
 	LightBean() {};
-	LightBean(const sf::Vector2f& center, const sf::Vector2f& radius) : center(center), radius(radius) {}
-	LightBean(const sf::Vector2f& center, float radius) : center(center), radius(sf::Vector2f(radius, radius)) {}
+	LightBean(const sf::Vector2f& center, const sf::Vector2f& radius) : center(center), radius(radius) {};
+	LightBean(const sf::Vector2f& center, const sf::Vector2f& radius, float level) : center(center), radius(radius), brightness(brightness) {};
+	LightBean(const sf::Vector2f& center, float radius) : center(center), radius(sf::Vector2f(radius, radius)) {};
+	LightBean(const sf::Vector2f& center, float radius, float level) : center(center), radius(sf::Vector2f(radius, radius)), brightness(brightness) {};
 
 	sf::Vector2f center;
 	sf::Vector2f radius;
+	float brightness = 1.f;
 };
