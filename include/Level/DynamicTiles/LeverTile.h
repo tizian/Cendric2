3add2c8f4a1780e6558bd10aeaccc68ae7ab68a1
Ironbell <isabelleroesch@msn.com>
2016-01-17 02:10:08 +0100

    more fixess

--- include/Level/DynamicTiles/LeverTile.h
+++ include/Level/DynamicTiles/LeverTile.h
@@ -24,7 +24,7 @@ private:
 	LevelMainCharacter* m_mainChar;
 	// the lever can only be used by hand if the main char is in this range.
 	// it is however always possible for a telekinesis spell to activate the lever if its projectile reaches the lever.
-	const float ACTIVATE_RANGE = 60.f;
+	const float ACTIVATE_RANGE = 100.f;
 	std::vector<SwitchableTile*> m_dependentTiles;
 
 	sf::Sound m_sound;
