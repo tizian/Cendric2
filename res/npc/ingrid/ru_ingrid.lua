414b9984904a26355926f7cddf72a2c615261964
Ironbell <isabelleroesch@msn.com>
2016-03-05 17:30:09 +0100

    intermediate commit

---
+++ res/npc/ingrid/ru_ingrid.lua
@@ -0,0 +1,46 @@
+-- Routine for NPC "Hunter Edmond"
+
+velocity = 50
+
+loadRoutine = function(R)
+		if (R:isConditionFulfilled("default","tutorial_complete")) then 
+		
+			if (R:isConditionFulfilled("hunter_edmond", "talked_after_tutorial")) then
+				R:setTilePosition(57, 17)
+
+				R:goToTile(57,17)
+				R:goToTile(65,17)
+				R:goToTile(65,14)
+				R:goToTile(66,14)
+				R:goToTile(66,12)
+				R:goToTile(61,12)
+				R:goToTile(61,13)
+				R:goToTile(60,13)
+				R:goToTile(60,17)
+			else 
+			
+				R:setTilePosition(57, 17)
+				R:setLooped(false)
+				R:setTalkingActive(true)
+				
+			end
+			
+		elseif (R:isConditionFulfilled("level_entry","tutorial_started")) then 
+		
+			R:setTilePosition(44, 17)
+			R:setTalkingEnabled(false)
+			
+			R:wait(1000)
+			R:goToTile(46,18)
+			R:setLooped(false)
+			
+		else
+			R:setTalkingActive(true)
+			R:setTilePosition(44, 17)
+			
+			R:wait(1000)
+			R:goToTile(40,17)
+			R:wait(1000)
+			R:goToTile(44,17)
+		end
+    end
