c913439347ebb48fbaad27192e046e3c6abba8d8
Ironbell <isabelleroesch@msn.com>
2016-03-27 01:22:03 +0100

    added cutscene: SILKWEED POTION

--- db/insert_scripts/insert_text_cutscene_utf8.sql
+++ db/insert_scripts/insert_text_cutscene_utf8.sql
@@ -5,5 +5,21 @@ INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Wh
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Elder:HereDrink', 'cutscene','Here, take the potion and drink it as fast as possible.','Da, nimm den Trank und trink ihn so schnell wie möglich.','Da, nimm de Trank und trink ihn so schnell we möglich.');
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Elder:ItWillReveal', 'cutscene','It might feel a little bit strange, but it will bring clarity.','Es wird sich vielleicht etwas komisch anfühlen, aber der Trank wird Klarheit bringen.','Es wird sich vilicht chli komisch afühle aber de Trank wird Klarheit bringe.');
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:LetsDrink', 'cutscene','Okay, let''s get through this quickly.','Okay, bringen wir''s hinter uns.','Okay, bringemers hinter üs.');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:Cough', 'cutscene','*Cough!*','*Hust!*','*Huest!*');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:Aah', 'cutscene','Aaah!','Aaah!','Aaah!');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Crash', 'cutscene','*Crash*','*Splitter*','*Scherbel*');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:ItBurns', 'cutscene','It burns!','Es brennt!','Es brännt!');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:WhatsHappening', 'cutscene','What''s happening with me...','Was passiert mit mir...','Was passiert mit mir...');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:IFeelSomething', 'cutscene','There, I feel something...','Da, ich spüre etwas...','Da, ich gspühr öppis...');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:SoCold', 'cutscene','So cold...','So kalt...','So chalt...');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:AmI', 'cutscene','Is this... myself?','Bin das... ich?','Bi das... ich?');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:AMage', 'cutscene','A mage, that''s what I was.','Ein Magier war ich also.','Ich bin also en Magier gsi.');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:TheVisionOver', 'cutscene','The vision is over.','Die Vision ist vorbei.','D''Vision isch verbii.');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:TheWaterFrozen', 'cutscene','The water... it''s frozen.','Das Wasser... Es ist gefroren.','Es Wasser... Es isch gfrore.');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:IDidThat', 'cutscene','I think I did that.','Ich glaube, das war ich.','Ich glaub, da bin ich gsi.');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:WhatsThis', 'cutscene','But, what''s this?!','Aber, was ist das?!','Aber, was isch das?!');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:AMark', 'cutscene','That mark...','Dieses Zeichen...','Da Zeiche...');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Cendric:WasntHereBefore', 'cutscene','... It wasn''t there before!','Es war vorher noch nicht da!','Da isch vorene nanig da gsi!');
+
 
 
--- res/cutscene/silkweed_potion/silkweed_potion.lua
+++ res/cutscene/silkweed_potion/silkweed_potion.lua
@@ -21,6 +21,82 @@ steps =
 			{"1_i_2.png", 10, 270},
 			{"2_i_3.png", 15, 270},
 		}
+	},
+	{
+		{
+			{"Cendric:Cough", 2},
+			{"Cendric:Aah", 2},
+		},
+		{
+			{"3_i_1.png", 0, 0},
+			{"3_i_2.png", 120, 180}
+		}
+	},
+	{
+		{
+			{"Crash", 4}
+		},
+		{
+			{"4_i_1.png", 0, 0},
+			{"4_i_2.png", 30, 315},
+			{"4_i_3.png", 30, 45}
+		}
+	},
+	{
+		{
+			{"Cendric:ItBurns", 3},
+			{"Cendric:WhatsHappening", 4}
+		},
+		{
+			{"5_i_1.png", 0, 0},
+			{"5_i_2.png", 50, 180},
+			{"5_i_3.png", 100, 180}
+		}
+	},
+	{
+		{
+			{"Cendric:IFeelSomething", 3},
+			{"Cendric:SoCold", 2}
+		},
+		{
+			{"6_i_1.png", 0, 0}
+		}
+	},
+	{
+		{
+			{"Cendric:AmI", 3},
+			{"Cendric:AMage", 7}
+		},
+		{
+			{"6_i_1.png", 0, 0},
+			{"6_i_2.png", 15, 0},
+			{"6_i_3.png", 20, 0},
+			{"6_i_4.png", 40, 10},
+			{"6_i_5.png", 40, 350}
+		}
+	},
+	{
+		{
+			{"Cendric:TheVisionOver", 3},
+			{"Cendric:TheWaterFrozen", 4},
+			{"Cendric:IDidThat", 4},
+			{"Cendric:WhatsThis", 3}
+		},
+		{
+			{"7_i_1.png", 0, 0},
+			{"7_i_2.png", 10, 0},
+			{"5_i_3.png", 150, 0}
+		}
+	},
+	{
+		{
+			{"Cendric:AMark", 3},
+			{"Cendric:WasntHereBefore", 4}
+		},
+		{
+			{"8_i_1.png", 0, 0},
+			{"8_i_2.png", 10, 0}
+		}
 	}
 }
 
