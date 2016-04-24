504977a543b7b0c9f7137de339517aff7a9e1cec
Ironbell <isabelleroesch@msn.com>
2016-04-24 16:35:09 +0200

    added texts for hint titles (type: hint) and changed type of hint description (type: hint_desc)

--- db/insert_scripts/insert_text_hint_utf8.sql
+++ db/insert_scripts/insert_text_hint_utf8.sql
@@ -1,17 +1,45 @@
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Press', 'hint','Press','Drücke die Taste','Druck d''Taste');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Journal', 'hint','to open the journal.','um das Tagebuch zu öffnen.','zums Tagebuech ufmache.');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Inventory', 'hint','to open the inventory.','um das Inventar zu öffnen.','zums Inventar ufmache.');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Chop', 'hint','to chop with your weapon. You can also use a left mouse klick, if that attack is selected.','um mit deiner Waffe zu schlagen. Du kannst auch einen linken Mausklick verwenden, wenn diese Attacke ausgwählt ist.','zum mit dinere Waffe haue. Du chasch au en lingge Muusklick verwende, wenn die Attacke usgwählt isch.');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('LeaveLevel', 'hint','when standing at a level exit to leave the level.','wenn du bei einem Level-Ausgang stehst, um das Level zu verlassen.','wenn du vor emne Level-Usgang stahsch zum use gah.');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Scout', 'hint','to scout.','um einen besseren Überblick zu bekommen.','zum en bessere Überblick z''ha.');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Map', 'hint','to open the map.','um die Karte zu öffnen.','zum d''Charte ufmache.');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('MapMove', 'hint','to move.','um dich zu bewegen.','zum dich bewege.');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Pickup', 'hint','Click on an item with the right mouse key to pickup.','Klicke einen Gegenstand mit der rechten Maustaste an, um ihn aufzuheben.','Klick en Gegestand mit de rechte Muustaschte ah zum ihn neh.');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Highlight', 'hint','to highlight items that can be picked up.','um aufhebbare Gegenstände zu markieren.','zum ufhebbari Gegeständ markiere.');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Reload', 'hint','Press the Escape key to go back to a previous checkpoint and reload the level.','Drücke Escape um zu einem vorherigen Kontrollpunkt zu gehen und das Level neu zu laden.','Druck Escape zum zumne vorherige Checkpoint zrugg z''ga und es Level neu z''lade.');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Spellbook', 'hint','to open up the spellbook and equip your new spell.','um das Zauberbuch zu öffnen und deinen neuen Zauber auszurüsten.','zum es Zauberbuech ufmache und din neue Zauber uusz''rüste');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Checkpoint', 'hint','Activating a checkpoint with a right or left click will allow you to save your game and return to its position when you die.','Das Spiel kann an einem Kontrollpunkt gespeichert werden indem du ihn mit einem Links- oder Rechtsklick aktivierst. Du kannst dich dorthin zurückversetzen, falls du stirbst.','Es Spiel cha amne Checkpoint gspeicheret werde wenn du ihn mitme Links- oder Rechtsklick aktiviersch und du chasch det ane zrugg ga falls du verrecksch.');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Modifier', 'hint','Magical gems alter your spells. You can equip them outside of a level using your spellbook.','Magische Edelsteine verändern deine Zauber. Du kannst sie im Zauberbuch ausserhalb eines Levels ausrüsten.','Magischi Edelset verändered dini Zauber. Du chasch si im Zauberbuech usserhalb vomne Level usrüste.');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Waypoint', 'hint','After you''ve activated a waypoint, you can take a teleport back, as long as you''re in the same region. Use a mouse click on the map or the minimap to teleport.','Nachdem du einen Wegpunkt aktiviert hast, kannst du dich dorthin zurück teleportieren, sofern du dich im selben Gebiet befindest. Teleportiere dich mit einem Mausklick auf der Karte zu einem Wegpunkt.','Wenn du en Wägpunkt aktiviert hesch, chasch dich det ane zrugg teleportiere, sofern du im gliiche Gebiet bisch. Teleportier dich mitme Muusklick uf de Karte zumne Wägpunkt.');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Press', 'hint_desc','Press','Drücke die Taste','Druck d''Taste');
+
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Journal', 'hint','Journal','Tagebuch','Tagebuech');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Journal', 'hint_desc','to open the journal.','um das Tagebuch zu öffnen.','zums Tagebuech ufmache.');
+
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Inventory', 'hint','Inventory','Inventar','Inventar');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Inventory', 'hint_desc','to open the inventory.','um das Inventar zu öffnen.','zums Inventar ufmache.');
+
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Chop', 'hint','Attack (Chop)','Attackieren (Schlagen)','Attackiere (Haue)');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Chop', 'hint_desc','to chop with your weapon. You can also use a left mouse klick, if that attack is selected.','um mit deiner Waffe zu schlagen. Du kannst auch einen linken Mausklick verwenden, wenn diese Attacke ausgwählt ist.','zum mit dinere Waffe haue. Du chasch au en lingge Muusklick verwende, wenn die Attacke usgwählt isch.');
+
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('LeaveLevel', 'hint','Leave Level','Level verlassen','Level verlah');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('LeaveLevel', 'hint_desc','when standing at a level exit to leave the level.','wenn du bei einem Level-Ausgang stehst, um das Level zu verlassen.','wenn du vor emne Level-Usgang stahsch zum use gah.');
+
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Scout', 'hint','Scouting','Kamera im Level','Kamera im Level');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Scout', 'hint_desc','to scout.','um einen besseren Überblick zu bekommen.','zum en bessere Überblick z''ha.');
+
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Map', 'hint','Minimap','Karte','Charte');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Map', 'hint_desc','to open the minimap.','um die Karte zu öffnen.','zum d''Charte ufmache.');
+
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('MapMove', 'hint','Movement on the map','Bewegen auf der Karte','Bewege uf de Charte');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('MapMove', 'hint_desc','to move.','um dich zu bewegen.','zum dich bewege.');
+
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Pickup', 'hint','Item pickup','Gegenstände aufnehmen','Gegeständ ufnä');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Pickup', 'hint_desc','Click on an item with the right mouse key to pickup.','Klicke einen Gegenstand mit der rechten Maustaste an, um ihn aufzuheben.','Klick en Gegestand mit de rechte Muustaschte ah zum ihn neh.');
+
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Highlight', 'hint','Highlight items','Gegenstände markieren','Gegeständ markiere');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Highlight', 'hint_desc','to highlight items that can be picked up.','um aufhebbare Gegenstände zu markieren.','zum ufhebbari Gegeständ markiere.');
+
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Reload', 'hint','Reload level','Level neu laden','Level neu lade');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Reload', 'hint_desc','Press the Escape key to go back to a previous checkpoint and reload the level.','Drücke Escape um zu einem vorherigen Kontrollpunkt zu gehen und das Level neu zu laden.','Druck Escape zum zumne vorherige Checkpoint zrugg z''ga und es Level neu z''lade.');
+
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Spellbook', 'hint','Spellbook','Zauberbuch','Zauberbuech');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Spellbook', 'hint_desc','to open up the spellbook and equip your new spell.','um das Zauberbuch zu öffnen und deinen neuen Zauber auszurüsten.','zum es Zauberbuech ufmache und din neue Zauber uusz''rüste');
+
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Checkpoint', 'hint','Checkpoint','Kontrollpunkt','Checkpoint');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Checkpoint', 'hint_desc','Activating a checkpoint with a right or left click will allow you to save your game and return to its position when you die.','Das Spiel kann an einem Kontrollpunkt gespeichert werden indem du ihn mit einem Links- oder Rechtsklick aktivierst. Du kannst dich dorthin zurückversetzen, falls du stirbst.','Es Spiel cha amne Checkpoint gspeicheret werde wenn du ihn mitme Links- oder Rechtsklick aktiviersch und du chasch det ane zrugg ga falls du verrecksch.');
+
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Modifier', 'hint','Magical gems','Magische Edelsteine','Magischi Edelstei');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Modifier', 'hint_desc','Magical gems alter your spells. You can equip them outside of a level using your spellbook.','Magische Edelsteine verändern deine Zauber. Du kannst sie im Zauberbuch ausserhalb eines Levels ausrüsten.','Magischi Edelstei verändered dini Zauber. Du chasch si im Zauberbuech usserhalb vomne Level usrüste.');
+
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Waypoint', 'hint','Waypoint','Wegpunkt','Wägpunkt');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Waypoint', 'hint_desc','After you''ve activated a waypoint, you can take a teleport back, as long as you''re in the same region. Use a mouse click on the map or the minimap to teleport.','Nachdem du einen Wegpunkt aktiviert hast, kannst du dich dorthin zurück teleportieren, sofern du dich im selben Gebiet befindest. Teleportiere dich mit einem Mausklick auf der Karte zu einem Wegpunkt.','Wenn du en Wägpunkt aktiviert hesch, chasch dich det ane zrugg teleportiere, sofern du im gliiche Gebiet bisch. Teleportier dich mitme Muusklick uf de Karte zumne Wägpunkt.');
 
 
