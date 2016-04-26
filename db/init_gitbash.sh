6d24e3fd197e09ee874cf6b6b9f7842fe9d0656a
tizian <tizian.zeltner@gmail.com>
2016-04-26 20:06:22 +0200

    Add gitbash db init script Tiny scroll sprite fixes

---
+++ db/init_gitbash.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+# game data
+./SQLiteShell.exe game_data.db '.read init_game_data.sql'
+for f in $(find insert_scripts -name '*.sql')
+do 
+	echo 'reading '$f'...'
+	./SQLiteShell.exe game_data.db '.read '$f
+	echo 'read '$f
+done
