534298943db54cad7e7b572246369a9b0a4ffcad
Ironbell <isabelleroesch@msn.com>
2016-04-27 23:51:17 +0200

    intermediate commit

---
+++ include/Level/DynamicTiles/ShootingTile.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "global.h"
+#include "Level/LevelDynamicTile.h"
+#include "MovableGameObject.h"
+#include "Structs/ShootingTileData.h"
+#include "Structs/DamageOverTimeData.h"
+
+class ShootingTile : public virtual LevelDynamicTile {
+public:
+	ShootingTile(LevelScreen* levelScreen);
+
+	void init() override;
+	void loadAnimation(int skinNr) override;
+	void onHit(Spell* spell) override;
+	void update(const sf::Time& frameTime) override;
+	void onHit(LevelMovableGameObject* mob) override;
+	void setShootingTileData(const ShootingTileData& data);
+
+private:
+	const float GRAVITY_ACCELERATION = 1000.f;
+	const float AGGRO_DISTANCE = 200.f;
+	
+	sf::Vector2f m_initialVelocity;
+	sf::Time m_waitingSpan = sf::Time::Zero;
+	sf::Time m_waitingTime = sf::Time::Zero;
+	sf::Time m_damageCooldown = sf::Time::Zero;
+	bool m_isWaiting = false;
+	bool m_isAggro = false;
+	bool m_isAlternating = false;
+	bool m_isReturning = false;
+	bool m_isMelting = false;
+	DamageOverTimeData m_damage;
+	sf::Vector2f m_initialPosition;
+};
---
+++ src/Level/DynamicTiles/ShootingTile.cpp
@@ -0,0 +1,84 @@
+#include "Level/DynamicTiles/ShootingTile.h"
+#include "Spells/Spell.h"
+#include "Registrar.h"
+#include "Screens/WorldScreen.h"
+
+REGISTER_LEVEL_DYNAMIC_TILE(LevelDynamicTileID::Shooting, ShootingTile)
+
+ShootingTile::ShootingTile(LevelScreen* levelScreen) :
+	LevelDynamicTile(levelScreen) {
+}
+
+void ShootingTile::init() {
+	setSpriteOffset(sf::Vector2f(-10.f, -10.f));
+	setPositionOffset(sf::Vector2f(10.f, 10.f));
+	setBoundingBox(sf::FloatRect(0.f, 0.f, TILE_SIZE_F - 20.f, TILE_SIZE_F - 20.f));
+	m_damage.damageType = DamageType::Physical;
+	m_damage.duration = sf::seconds(4.f);
+	m_damage.damage = 40;
+	m_isAlwaysUpdate = true;
+}
+
+void ShootingTile::loadAnimation(int skinNr) {
+	m_isCollidable = false;
+
+	Animation* idleAnimation = new Animation();
+	idleAnimation->setSpriteSheet(g_resourceManager->getTexture(ResourceID::Texture_tile_jumping));
+	for (int i = 0; i < 3; ++i) {
+		idleAnimation->addFrame(sf::IntRect(TILE_SIZE * i, (skinNr - 1) * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+	}
+
+	addAnimation(GameObjectState::Idle, idleAnimation);
+
+	// initial values
+	setState(GameObjectState::Idle);
+	playCurrentAnimation(true);
+
+	if (skinNr == 2) { // fireball
+		m_damage.damageType = DamageType::Fire;
+		m_isMelting = true;
+	}
+}
+
+void ShootingTile::onHit(LevelMovableGameObject* mob) {
+	if (m_damageCooldown > sf::Time::Zero) return;
+	if (mob->getConfiguredType() == GameObjectType::_LevelMainCharacter) {
+		mob->addDamageOverTime(m_damage);
+		m_damageCooldown = sf::seconds(3.f);
+	}
+}
+
+void ShootingTile::setShootingTileData(const ShootingTileData& data) {
+	m_isAggro = data.isAggro;
+	m_isAlternating = data.isAlternating;
+
+	float phi = degToRad(static_cast<float>(data.direction - 90));
+
+	m_initialVelocity.x = std::round(data.velocity * std::cos(phi));
+	m_initialVelocity.y = std::round(data.velocity * std::sin(phi));
+
+	m_waitingSpan = sf::milliseconds(data.waitingTime);
+	m_initialPosition = data.spawnPosition;
+}
+
+void ShootingTile::update(const sf::Time& frameTime) {
+	LevelDynamicTile::update(frameTime);
+	updateTime(m_damageCooldown, frameTime);
+
+	if (m_isWaiting) {
+		if (m_isAggro && dist(m_mainChar->getCenter(), getCenter()) > AGGRO_DISTANCE) {
+			return;
+		}
+		updateTime(m_waitingTime, frameTime);
+		if (m_waitingTime == sf::Time::Zero) {
+			m_isWaiting = false;
+		}
+		return;
+	}
+}
+
+void ShootingTile::onHit(Spell* spell) {
+	// nop, yet
+}
+
+
