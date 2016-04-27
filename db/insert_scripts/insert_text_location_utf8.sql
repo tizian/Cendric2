6e6bdfb3553b90c4650195aa193e50e8cec20ddb
Ironbell <isabelleroesch@msn.com>
2016-04-27 22:56:46 +0200

    small dialogue fixes

--- db/insert_scripts/insert_text_location_utf8.sql
+++ db/insert_scripts/insert_text_location_utf8.sql
@@ -1,5 +1,5 @@
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Meadows','location','The Meadows','Mittelland','Mittelland');
-INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('RatCave','location','The Rats Nest','Das Rattennest','Es Rattenest');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('RatCave','location','The Rats'' Nest','Das Rattennest','Es Rattenest');
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Forest','location','The Forest','Der Wald','De Wald');
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Rockfall','location','The Rockfall','Der Felssturz','De Felssturz');
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('Crypt','location','Crypt','Krypta','Krypta');
