e7b15be6b53f453abeb54280cc212450515ab36d
tizian <tizian.zeltner@gmail.com>
2016-05-26 22:38:23 +0200

    Add chapter 1 title to first cutscene

--- res/cutscene/intro/intro.lua
+++ res/cutscene/intro/intro.lua
@@ -12,12 +12,21 @@ steps =
 	{
 		{
 			{"WhatHappened", 5},
-			{"WhereAmI", 6}
+			{"WhereAmI", 5}
 		},
 		{
 			{"forest_background.png", 0, 0},
 			{"forest_foreground.png", 10, 95},
 			{"forest_trees.png", 40, 80},
+		}
+	},
+	{
+		{
+			{"", 3}
+		},
+		{
+			{"darkness.png", 0, 0},
+			{"chapter1.png", 0, 0}
 		}
 	}
 }
