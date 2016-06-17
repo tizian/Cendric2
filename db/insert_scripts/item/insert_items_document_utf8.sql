43eb4db28de66adca78f5eee67612cd2bf0a4e3a
Ironbell <isabelleroesch@msn.com>
2016-06-17 23:47:43 +0200

    intermediate commit

--- db/insert_scripts/item/insert_items_document_utf8.sql
+++ db/insert_scripts/item/insert_items_document_utf8.sql
@@ -7,5 +7,5 @@ INSERT INTO levelitem_frame (item_id, frame_nr, frame_left, frame_top, frame_wid
 -- Note (Paladin Marcus)
 
 INSERT INTO item (item_id, item_type, icon_left, icon_top, gold_value) values ('do_notemarcus', 'document', 400, 400, -1);
-INSERT INTO levelitem (item_id, sprite_offset_left, sprite_offset_top, bounding_box_width, bounding_box_height) values ('do_notemarcus', 25, -30, 50, 50);
+INSERT INTO levelitem (item_id, sprite_offset_left, sprite_offset_top, bounding_box_width, bounding_box_height) values ('do_notemarcus', -25, 0, 50, 50);
 INSERT INTO levelitem_frame (item_id, frame_nr, frame_left, frame_top, frame_width, frame_height) values ('do_notemarcus', 1, 450, 0, 100, 50);
