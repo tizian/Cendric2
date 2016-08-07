d1ece591f6271d59aa74cd1e077316258dbca775
Ironbell <isabelleroesch@msn.com>
2016-08-07 22:54:47 +0200

    updated dialogue for stephen updated city wall in cutscene

--- res/npc/npc_stephen/dl_npc_stephen.lua
+++ res/npc/npc_stephen/dl_npc_stephen.lua
@@ -9,7 +9,7 @@ loadDialogue = function(DL)
 
 	if (not DL:isConditionFulfilled("npc_stephen", "talked")) then 
 
-		DL:createNPCNode(1, 3, "DL_NPC_ANewFace") -- And again, a new face in the city. As if we hadn't already troublemakers here. 
+		DL:createNPCNode(1, 3, "DL_NPC_ANewFace") -- And again, a new face in the city. As if we hadn't already enough troublemakers here. 
 		DL:addConditionProgress("npc_stephen", "talked")
 		DL:addNode()
 
@@ -21,24 +21,39 @@ loadDialogue = function(DL)
 
 
 	DL:createChoiceNode(2)
-	DL:addChoice(5, "DL_Choice_ExplainRules") -- Explain me the rules of the city.
+	if (not DL:isConditionFulfilled("npc_stephen", "rules")) then 
+		DL:addChoice(5, "DL_Choice_ExplainRules") -- Explain me the rules of the city.
+	end
 	if (not DL:isConditionFulfilled("npc_stephen", "who_are_you")) then 
 		DL:addChoice(6, "DL_Choice_WhoAreYou") -- Who are you?
 	end
-	DL:addChoice(4, "DL_choice_NoTime") -- I don't have time for this.
+	if (not DL:isConditionFulfilled("npc_stephen", "rules")) then 
+		DL:addChoice(4, "DL_choice_NoTime") -- I don't have time for this.
+	end
+	if (DL:isConditionFulfilled("npc_stephen", "rules")) then 
+		DL:addChoice(-1, "DL_Choice_CU") -- See you.
+	end
 	DL:addNode()
 
+	if (not DL:isConditionFulfilled("npc_stephen", "rules")) then 
 
-	DL:createNPCNode(5, 9, "DL_NPC_Rules1") -- Well, first, you should know that the Order of the Eternal Light is in charge here. Therefore, respect us and follow our orders as long as you're here, or it won't be a very pleasant stay in Gandria.
-	DL:addNode()
-
-
-	DL:createNPCNode(9, 10, "DL_NPC_Rules2") -- Behave well if you go into a stranger's house - we don't like thieves here. Most citicens of Gandria use observer spells to avoid being robbed.
-	DL:addNode()
+		DL:createNPCNode(5, 9, "DL_NPC_Rules1") -- Well, first, you should know that the Order of the Eternal Light is in charge here. Therefore, respect us and follow our orders as long as you're here, or it won't be a very pleasant stay in Gandria.
+		DL:addNode()
 
 
-	DL:createNPCNode(10, -2, "DL_NPC_Rules3") -- And, last but not least, we don't really like seeing other spells than those which originate from the high divine art. You can hang for practicing Necromancy.
-	DL:addNode()
+		DL:createNPCNode(9, 10, "DL_NPC_Rules2") -- Behave well if you go into a stranger's house - we don't like thieves here. Most citicens of Gandria use observer spells to avoid being robbed.
+		DL:addNode()
+
+
+		DL:createNPCNode(10, 11, "DL_NPC_Rules3") -- And, last but not least, we don't really like seeing other spells than those which originate from the high divine art. You can hang for practicing Necromancy.
+		DL:addNode()
+
+
+		DL:createCendricNode(11, -2, "DL_Cendric_Okay") -- Okay.
+		DL:addConditionProgress("npc_stephen", "rules")
+		DL:addNode()
+
+	end
 
 	if (not DL:isConditionFulfilled("npc_stephen", "who_are_you")) then 
 
@@ -50,7 +65,7 @@ loadDialogue = function(DL)
 		DL:createChoiceNode(7)
 		DL:addChoice(-2, "DL_choice_Understood") -- Understood.
 		DL:addChoice(-2, "DL_Choice_MaybeUnderstood") -- I'll try.
-		DL:addChoice(8, "DL_Choice_NotUnderstood") -- ... (Grin disparagingly)
+		DL:addChoice(8, "DL_Choice_NotUnderstood") -- ... (Mischievous grin)
 		DL:addNode()
 
 
@@ -59,8 +74,14 @@ loadDialogue = function(DL)
 
 	end
 
+	if (not DL:isConditionFulfilled("npc_stephen", "rules")) then 
 
-	DL:createNPCNode(4, -1, "DL_NPC_DontTrouble") -- Well then... Don't make trouble!
-	DL:addNode()
+		DL:createNPCNode(4, -1, "DL_NPC_DontTrouble") -- Well then... Don't make trouble!
+		DL:addNode()
+
+	end
+
+	if (DL:isConditionFulfilled("npc_stephen", "rules")) then 
+	end
 
 end
--- res/npc/npc_stephen/ru_npc_stephen.lua
+++ res/npc/npc_stephen/ru_npc_stephen.lua
@@ -5,8 +5,8 @@ loadRoutine = function(R, W)
 	if (not W:isConditionFulfilled("npc_stephen", "talked")) then 
 		R:setTalkingActive(true) 
 	end 
-	R:setTilePosition(4,62.7)
-	R:goToTile(4,64)
+	R:setTilePosition(4,61.7)
+	R:goToTile(4,63)
 	R:setLooped(false)
 	
 end	
