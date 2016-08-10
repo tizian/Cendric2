49bde504e3fd2a14e3e70d7deffadfa6c701a440
Ironbell <isabelleroesch@msn.com>
2016-08-10 12:41:50 +0200

    added zeff to gandria (no dialogue yet)

--- db/insert_scripts/insert_text_npc_utf8.sql
+++ db/insert_scripts/insert_text_npc_utf8.sql
@@ -14,6 +14,7 @@ INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('np
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('npc_tristan','npc','Tristan','Tristan','Tristan');
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('npc_zeff','npc','Zeff','Zeff','Zeff');
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('npc_zeff2','npc','Zeff','Zeff','Zeff');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('npc_zeff3','npc','Zeff','Zeff','Zeff');
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('npc_cityguard1','npc','City Guard','Stadtwache','Stadtwach');
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('npc_cityguard2','npc','City Guard','Stadtwache','Stadtwach');
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('npc_stephen','npc','Stephen','Stephen','Stephen');
