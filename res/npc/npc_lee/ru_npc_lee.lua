42fb97708c1fdde35cb2d424195d6eb5c5606099
Ironbell <isabelleroesch@msn.com>
2017-01-27 22:44:49 +0100

    added routines for lee and luv

--- res/npc/npc_lee/ru_npc_lee.lua
+++ res/npc/npc_lee/ru_npc_lee.lua
@@ -9,7 +9,29 @@ loadRoutine = function(R, W)
 	else
 		R:setTilePosition(62,54)
 		R:setFacingDown()
-		R:setLooped(false)
+		R:wait(2000)
+		
+		R:goToTile(57,54)
+		R:goToTile(57,60)
+		R:setFacingDown()
+		R:wait(4000)
+		
+		R:goToTile(57,58)
+		R:goToTile(58,58)
+		R:setFacingRight()
+		R:wait(5000)
+		
+		R:goToTile(57,54)
+		R:goToTile(50,54)
+		R:goToTile(50,60)
+		R:goToTile(51,60)
+		R:setFacingDown()
+		R:wait(8000)
+		
+		R:goToTile(50,54)
+		R:goToTile(50,60)
+		R:goToTile(57,54)
+		R:goToTile(62,54)
 	end
 
 end	
--- res/npc/npc_luv/ru_npc_luv.lua
+++ res/npc/npc_luv/ru_npc_luv.lua
@@ -8,8 +8,25 @@ loadRoutine = function(R, W)
 		R:setDisposed()
 	else
 		R:setTilePosition(68,58)
+		
+		R:goToTile(66,58)
+		R:setFacingLeft()
+		R:wait(5000)
+
+		R:goToTile(68,58)
+		R:goToTile(68,52)
+		R:goToTile(73,52)
+		R:goToTile(73,56)
 		R:setFacingDown()
-		R:setLooped(false)
+		R:wait(8000)
+		
+		R:goToTile(73,53)
+		R:goToTile(68,53)
+		R:goToTile(68,60)
+		R:setFacingDown()
+		R:wait(3000)
+		
+		R:goToTile(68,58)
 	end
 
 end	
