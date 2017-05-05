3f2a970b171a1eaa4bbae01b18d7e4b110057409
Ironbell <isabelleroesch@msn.com>
2017-05-05 23:29:30 +0200

    added template for chapt 4 cutscene

---
+++ res/cutscene/chapter_4/chapter_4.lua
@@ -0,0 +1,14 @@
+-- lua script for cutscene chapter_4
+steps = {
+	
+	{
+		texts = {
+			{str="Chapter4", time=2, centered=true}
+		},
+		images = {
+			{path="black.png", vel=0, angle=0},
+			{path = {en="chapter4_en.png", de="chapter4_de.png", ch="chapter4_de.png", es="chapter4_es.png"}, vel=0, angle=0}
+		},
+		fadetime=1.0
+	}
+}
