ccabd7ce265563819212945dd443c2a03b85fb86
Ironbell <isabelleroesch@msn.com>
2017-05-08 22:00:00 +0200

    added template for level: THE ASCENT, testing snow weather

--- db/insert_scripts/insert_text_location_utf8.sql
+++ db/insert_scripts/insert_text_location_utf8.sql
@@ -52,3 +52,4 @@ INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('ga
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('boss_mercenaries','location','Boss: Mercenaries','Boss: Söldner','Boss: Söldner');
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('highlands','location','Highlands of Admantris','Hochland von Admantris','Hochland vo Admantris');
 INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('swampforest','location','The Rainy Forest','Der Verregnete Wald','De Verregnet Wald');
+INSERT INTO text (text_id, text_type, english, german, swiss_german) values ('ascent','location','The Ascent','Der Aufstieg','De Ufstiig');
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include "TextProvider.h"
 
 // Create savegame and screenshot folders outside of the Cendric directory at locations determined by the operating system. (e.g. User/Documents/Cendric on Windows or ~/Library/Application Support/Cendric on Mac)
-#define EXTERNAL_DOCUMENTS_FOLDER
+// #define EXTERNAL_DOCUMENTS_FOLDER
 
 // Resolve .app package internal resource path for mac builds
 // #define APPLE_APP_BUILD
