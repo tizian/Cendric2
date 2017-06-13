539165571d99e563bb3ced0b96db75c6cb9c07ae
Ironbell <isabelleroesch@msn.com>
2017-06-13 23:41:39 +0200

    missing file

---
+++ include/Beans/ItemEquipmentParticleBean.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include "Beans/DatabaseBean.h"
+
+// an equipment particle bean, directly from the database
+struct ItemEquipmentParticleBean final : DatabaseBean {
+	std::string item_id;
+	int particle_count;
+    bool is_additive_blend_mode;
+    float emit_rate;
+    std::string texture_path;
+    float spawner_radius;
+    sf::Vector2f spawner_offset;
+    float size_start_min;
+    float size_start_max;
+    float size_end_min;
+    float size_end_max;
+    sf::Color color_start_min;
+    sf::Color color_start_max;
+    sf::Color color_end_min;
+    sf::Color color_end_max;
+    sf::Vector2f goal_offset;
+    float speed_min;
+    float speed_max;
+    float time_min;
+    float time_max;
+};
