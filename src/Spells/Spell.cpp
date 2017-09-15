8381c7150aa4562a20a562b4a93ddf8718c01e70
Ironbell <isabelleroesch@msn.com>
2017-09-16 00:05:08 +0200

    fixed a khajag bug

--- src/Spells/Spell.cpp
+++ src/Spells/Spell.cpp
@@ -22,6 +22,7 @@ void Spell::load(const SpellData& data, LevelDynamicTile* tile, const sf::Vector
 }
 
 void Spell::initialize(const SpellData& data, GameObject* go, const sf::Vector2f& target) {
+	m_isAlwaysUpdate = true;
 	m_data = data;
 	setBoundingBox(data.boundingBox);
 	setDebugBoundingBox(COLOR_BAD);
