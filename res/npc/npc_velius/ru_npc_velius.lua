f1dc2dac8a9ce4ccfff304d501c1ec153ce4676d
Ironbell <isabelleroesch@msn.com>
2017-11-03 23:40:20 +0100

    started with velius dialogue

--- res/npc/npc_velius/ru_npc_velius.lua
+++ res/npc/npc_velius/ru_npc_velius.lua
@@ -7,6 +7,8 @@ loadRoutine = function(R, W)
 	R:setTilePosition(12.5,8)
 	R:setReloadEnabled(false)
     R:setLooped(false)
+    
+    R:setTalkingActive(not W:isConditionFulfilled("npc_velius", "talked"))
 	
 	R:setFacingDown()
 
