5108e869495d3b1f15b07580dcd1bbb08ef7fa67
Ironbell <isabelleroesch@msn.com>
2017-11-11 10:40:49 +0100

    fixes for edmonds behavior inside rockfall: fix #34 fix #35

--- res/level/rockfall/hunter.lua
+++ res/level/rockfall/hunter.lua
@@ -5,7 +5,9 @@ goodJob = false
 update = function(B, W)
 		if (not goodJob and W:isConditionFulfilled("npc_edmond","puzzle_solved")) then
 			goodJob = true
-			B:say("GoodJob", 4)
+            if (B:getPosX() > 850) then
+                B:say("GoodJob", 4)
+            end
 		elseif (speechBubbleState == 5) then
 			B:leaveLevel()
 		elseif (speechBubbleState == 4 and B:getPosX() > 2650) then
@@ -14,11 +16,13 @@ update = function(B, W)
 			B:wait(5)
 			B:setDisposed()
 			speechBubbleState = speechBubbleState + 1
-		elseif (speechBubbleState == 3 and B:getPosX() > 2000) then
+		elseif (speechBubbleState == 3 and B:getPosX() > 1800) then
 			B:say("KillRats", 6)
 			speechBubbleState = speechBubbleState + 1
 		elseif (speechBubbleState == 2 and B:getPosX() > 1450) then
-			B:say("FindAWay", 6)
+            if (not W:isConditionFulfilled("npc_edmond","puzzle_solved")) then
+                B:say("FindAWay", 6)
+            end
 			speechBubbleState = speechBubbleState + 1
 		elseif (speechBubbleState == 1 and B:getPosX() > 850) then
 			B:say("SpikesAreBad", 5)
