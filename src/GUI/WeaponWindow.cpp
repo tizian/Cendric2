b1c69d2c29c883f19c146a20003948b400c444c8
Ironbell <isabelleroesch@msn.com>
2018-02-06 23:23:11 +0100

    polish

--- src/GUI/WeaponWindow.cpp
+++ src/GUI/WeaponWindow.cpp
@@ -83,7 +83,7 @@ void WeaponWindow::reload() {
 		slotNr++;
 		m_weaponSlots.push_back(std::pair<SpellSlot*, std::vector<ModifierSlot*>>({ spellSlot, modifiers }));
 	}
-	if (m_selectedSpellSlot != nullptr && m_selectedSpellSlot->getNr() != -1 && static_cast<int>(m_weaponSlots.size()) > m_selectedSpellSlot->getNr()) {
+	if (m_selectedSpellSlot != nullptr && m_selectedSpellSlot->getNr() >= 0 && static_cast<int>(m_weaponSlots.size()) > m_selectedSpellSlot->getNr()) {
 		m_weaponSlots.at(m_selectedSpellSlot->getNr()).first->select();
 	}
 	else {
