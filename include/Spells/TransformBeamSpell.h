04518f4ffbb52ebc35c1a30a384028396013618f
Ironbell <isabelleroesch@msn.com>
2018-03-06 23:10:15 +0100

    small content fixes and fixed softlock

--- include/Spells/TransformBeamSpell.h
+++ include/Spells/TransformBeamSpell.h
@@ -14,6 +14,7 @@ public:
 	
 	void load(const SpellData& bean, LevelMovableGameObject* mob, const sf::Vector2f& target) override;
 	void update(const sf::Time& frameTime) override;
+	void execOnHit(LevelMovableGameObject* mob) override {};
 
 	bool getConfiguredRotateSprite() const override;
 
--- res/quest/antimagic_crystal.lua
+++ res/quest/antimagic_crystal.lua
@@ -11,13 +11,8 @@ markers = {
 	},
     {
 		map = "res/map/gandria/gandria.tmx",
-		position = {675, 1175},
-		step = 1
-	},
-    {
-		map = "res/map/gandria/gandria.tmx",
-		position = {675, 1175},
-		step = 1
+		position = {1325, 1025},
+		step = 0
 	},
 	{
 		map = "res/map/gandria/gandria.tmx",
