93e244b8ab479145610c8e6c70afc824e19a8c5b
Ironbell <isabelleroesch@msn.com>
2018-03-20 22:37:17 +0100

    gargoyle nerf

--- src/Level/Enemies/GargoyleEnemy.cpp
+++ src/Level/Enemies/GargoyleEnemy.cpp
@@ -41,7 +41,7 @@ void GargoyleEnemy::setSummoned(int strength, int count, const sf::Time& ttl) {
 }
 
 void GargoyleEnemy::loadAttributes() {
-	m_attributes.setHealth(400);
+	m_attributes.setHealth(250);
 	m_attributes.resistancePhysical = 400;
 	m_attributes.resistanceFire = 200;
 	m_attributes.resistanceLight = -50;
