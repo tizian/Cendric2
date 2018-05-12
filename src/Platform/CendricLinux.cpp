dc7abdbcfb0c63a1e58c8be1bc5bc888bca036c3
John <johnz@posteo.net>
2018-05-12 15:55:28 +0200

    Linux: initial Support for System-wide installation

---
+++ src/Platform/CendricLinux.cpp
@@ -0,0 +1,42 @@
+#include "Platform/CendricLinux.h"
+
+#ifdef __linux__
+
+#include <cstdlib>
+#include <sys/stat.h>
+#include <errno.h>
+#include <iostream>
+#include "Logger.h"
+
+std::string getExternalDocumentsPath() {
+	std::string resultPath = "";
+	std::string savePath = "";
+	if(const char* env_p = std::getenv("XDG_DATA_HOME"))
+		resultPath = std::string(env_p) + "/Cendric/";
+	else if(const char* env_p = std::getenv("HOME"))
+        	resultPath = std::string(env_p) + "/.local/share/Cendric/";
+	
+	savePath = resultPath + std::string("saves");
+	if(mkdir(resultPath.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == -1) {
+		int errorpc = errno;
+		if(errorpc != EEXIST) {
+			g_logger->logError("PlatformLinux", "Dir " + resultPath + " could not be created. Error: " + std::to_string(errorpc));
+		}
+	}
+	if(mkdir(savePath.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == -1) {
+		int errorpc = errno;
+		if(errorpc != EEXIST) {
+			g_logger->logError("PlatformLinux", "Dir " + savePath + " could not be created. Error: " + std::to_string(errorpc));
+		}
+	}
+
+	return resultPath;
+}
+
+std::string getSystemResourcePath() {
+	std::string resultPath = "/usr/share/Cendric/";
+	//maybe do some more stuff?
+	return resultPath;
+}
+
+#endif
