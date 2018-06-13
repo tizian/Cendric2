cc8c176a6ffbdb10b2d0ed3833154599ccc496d3
Ironbell <isabelleroesch@msn.com>
2018-06-13 23:14:01 +0200

    updated version nr to 1.1.0_beta

--- include/Global/global_definitions.h
+++ include/Global/global_definitions.h
@@ -17,7 +17,7 @@
 // #define STEAM
 
 // versioning
-#define CENDRIC_VERSION_NR_RAW "1.0.6"
+#define CENDRIC_VERSION_NR_RAW "1.1.0_beta"
 #define ENVIRONMENT "64bit"
 #ifdef STEAM
     #define CENDRIC_VERSION_NR CENDRIC_VERSION_NR_RAW " " ENVIRONMENT
